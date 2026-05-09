0f6333f5619789f1acd27ec2ad5032d03488afd4
Fabrice Nijimbere <4010650+FabMab@users.noreply.github.com>
2026-05-09 22:40:25 +0100

    feat: Add java solution for Kids With the Greatest Number of Candies

---
+++ easy/1528-kids-with-the-greatest-number-of-candies/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int findLargest(int[] candies){
+        //find largest element
+        //initiate max as first element
+        // Alternatively iterate & use Math.max
+        int max = candies[0];
+        for (int i= 0; i < candies.length; i++){
+             if(candies[i] > max){
+                //update max
+                max = candies[i];
+            }
+        } 
+        return max;
+    }         
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        ArrayList<Boolean> result = new ArrayList<>();
+        for (int i= 0; i < candies.length; i++){
+            int maxCandies = findLargest(candies);
+            if(candies[i] + extraCandies >= maxCandies){    
+                result.add(true);       
+            }else{
+                result.add(false);
+            } 
+        } 
+        return result;   
+    }
+}
