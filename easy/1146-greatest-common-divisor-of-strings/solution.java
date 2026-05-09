a7e2fc438f3c103952c7b97764a9844ffc475920
Fabrice Nijimbere <4010650+FabMab@users.noreply.github.com>
2026-05-09 23:18:04 +0100

    feat: Update java solution for Greatest Common Divisor of Strings

--- easy/1146-greatest-common-divisor-of-strings/solution.java
+++ easy/1146-greatest-common-divisor-of-strings/solution.java
@@ -1,23 +1,27 @@
 class Solution {
 
-    public String gcdOfStrings(String str1, String str2){
-        if (!(str1 +str2).equals(str2 +str1)){
-            return "";
-        }
+    public boolean valid(String s1, String s2, int k){
+        int s1Len = s1.length();
+        int s2Len = s2.length();
 
-        int lenGCD = gcd(str1.length(), str2.length());
-        return str1.substring(0, lenGCD);
-        
+        if (s1Len % k > 0 || s2Len % k > 0){
+            return false;
+        }else{
+            String base = s1.substring(0, k);
+            return s1.replace(base, "").isEmpty() && s2.replace(base, "").isEmpty();
+        }
     }
+    public String gcdOfStrings(String str1, String str2) {
 
-    public int gcd(int len1, int len2) {
+        int s1Len = str1.length();
+        int s2Len = str2.length();
 
-        while (len2 != 0){
-            int temp= len2;
-            len2 = len1 % len2;
-            len1 = temp;
+        for (int i = Math.min(s1Len, s2Len); i >=1; --i){
 
+            if (valid(str1, str2, i)){
+                return str1.substring(0, i);
+            }
         }
-        return len1;
+        return "";
     }
 }
