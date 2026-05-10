e944502412dc3e548b8d459384fadec3495e1b19
Fabrice Nijimbere <4010650+FabMab@users.noreply.github.com>
2026-05-10 22:56:04 +0100

    feat: Update java solution for Greatest Common Divisor of Strings

--- easy/1146-greatest-common-divisor-of-strings/solution.java
+++ easy/1146-greatest-common-divisor-of-strings/solution.java
@@ -1,27 +1,24 @@
 class Solution {
 
-    public boolean valid(String s1, String s2, int k){
-        int s1Len = s1.length();
-        int s2Len = s2.length();
-
-        if (s1Len % k > 0 || s2Len % k > 0){
-            return false;
-        }else{
-            String base = s1.substring(0, k);
-            return s1.replace(base, "").isEmpty() && s2.replace(base, "").isEmpty();
+    public String gcdOfStrings(String str1, String str2){
+        if (!(str1 +str2).equals(str2 +str1)){
+            return "";
         }
+
+        int lenGCD = gcd(str1.length(), str2.length());
+        return str1.substring(0, lenGCD);
+        
     }
-    public String gcdOfStrings(String str1, String str2) {
 
-        int s1Len = str1.length();
-        int s2Len = str2.length();
+    public int gcd(int len1, int len2) {
 
-        for (int i = Math.min(s1Len, s2Len); i >=1; --i){
+        while (len2 != 0){   // Euclidean Algorithm - Moves len2 into len1,replaces len2 with the remainder of len1 % len2 in each iteration
 
-            if (valid(str1, str2, i)){
-                return str1.substring(0, i);
-            }
+            int temp= len2;
+            len2 = len1 % len2;
+            len1 = temp;
+
         }
-        return "";
+        return len1;
     }
 }
